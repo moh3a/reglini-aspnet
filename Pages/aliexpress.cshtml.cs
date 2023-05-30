6831dc41ec0555b81869a02550e7a5f31dfe9bcf
moh3a <aitabdelmalekma@gmail.com>
2023-05-30 20:22:23 +0100

    Add project files.

---
+++ Pages/aliexpress.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspReglini.Pages
+{
+    public class aliexpressModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿const aeSearchForm = document.getElementById("aliexpress-search-form");
+
+aeSearchForm.addEventListener("submit", onFormSubmit);
+
+const onFormSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    console.log(data);
+};
